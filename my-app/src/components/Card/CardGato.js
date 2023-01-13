5098abc209d12013ab549633d9340a52ccf8c231
Tristan Lenzberg <112999910+tristanlgb@users.noreply.github.com>
2023-01-13 04:18:32 -0300

    Add files via upload

---
+++ my-app/src/components/Card/CardGato.js
@@ -0,0 +1,20 @@
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+// //cambiar nombre de basicexample a funcion propia
+// function CardGato() {
+//   return (
+//     <Card style={{ width: '18rem' }}>
+//       <Card.Img variant="top" src="./cat.jpg" />
+//       <Card.Body>
+//         <Card.Title>Gato </Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="success">Comprar mascota🐈</Button>
+//       </Card.Body>
+//     </Card>
+//   );
+// }
+
+// export default CardGato;
---
+++ my-app/src/components/Navbar/NavBar.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+const NavBar = () => {
+  return (
+    <div className='navbar-container'>NavBar</div>
+  )
+}
+
+export default NavBar;
